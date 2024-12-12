a34a9092bd5ed5adf75997657a2cd945de27b861
shreyajei <93199901+shreyajei@users.noreply.github.com>
2024-12-12 00:03:07 +0000

    Add files via upload

---
+++ Machine Learning/conversion.py
@@ -0,0 +1,8 @@
+with open('breastfeeding_model.h5', 'w') as header_file:
+    header_file.write("const unsigned char model[] = {\n")
+    with open("model.tflite", "rb") as tflite_file:
+        byte = tflite_file.read(1)
+        while byte:
+            header_file.write(f"0x{byte.hex()}, ")
+            byte = tflite_file.read(1)
+        header_file.write("\n};\n")
