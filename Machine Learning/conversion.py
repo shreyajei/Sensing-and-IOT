5b9715d205ab89cdc98edfe98d1b5cc89ecc87d6
shreyajei <93199901+shreyajei@users.noreply.github.com>
2024-12-12 00:04:05 +0000

    Update conversion.py

--- Machine Learning/conversion.py
+++ Machine Learning/conversion.py
@@ -1,3 +1,4 @@
+#90% AI generated, 10% self generated
 with open('breastfeeding_model.h5', 'w') as header_file:
     header_file.write("const unsigned char model[] = {\n")
     with open("model.tflite", "rb") as tflite_file:
